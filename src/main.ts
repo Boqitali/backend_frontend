f2fe110afd448291b8c6b692fe31e5af9aef6ab4
boqitali <ismonaliyevboqitali7@gmail.com>
2025-08-16 00:15:03 +0500

    Car tableliga barcha ma'lumotlar qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function start() {
   const app = await NestFactory.create(AppModule);
 
-  // CORS yoqish
+  
   app.enableCors();
 
   const PORT = process.env.PORT || 3000;
